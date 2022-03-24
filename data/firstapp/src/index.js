fdb4805c0ce9938a4a2f72a639e7c2b87f1f2851
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-03-24 22:42:40 +0100

    update' git push git commit -m update git push git commit -m "update

--- data/firstapp/src/index.js
+++ data/firstapp/src/index.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { OutlinedButtons, NormalButtons } from './navbar';
 
+function Welcome(props) {
+	return <h1>Hello, {props.name}</h1>;
+}
+
+class ShoppingList extends React.Component {
+	render() {
+		return (
+			<div className='shopping-list'>
+				<h1>Shopping List for {this.props.name}</h1>
+				<ul>
+					<li>Instagram</li>
+					<li>WhatsApp</li>
+					<li>Oculus</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+class NameForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '', ovalue: '' };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		this.setState({ ovalue: this.state.value });
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input type='text' value={this.state.value} onChange={this.handleChange} />
+				<input type='text' value={this.state.ovalue} onChange={this.handleChange} />
+				<input type='submit' value='Submit' />
+			</form>
+		);
+	}
+}
+
+const element1 = <ShoppingList name='Mark' />;
+
+const element = <Welcome name='Sara' />;
+
+//prettier-ignore
 ReactDOM.render(
-	<div class='container vh-100 vw-100 d-flex justify-content-center align-items-center'>
-		<OutlinedButtons />
-		<NormalButtons />
-	</div>,
+	[
+		element,
+		element1,
+		<NameForm />
+	],
 	document.getElementById('root')
 );
+
+//prettier-ignore
