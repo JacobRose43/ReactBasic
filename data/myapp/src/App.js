2f5bc782ae5fc293fdab2de9f9da4fdae429aeea
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-04-10 22:07:58 +0200

    update

--- data/myapp/src/App.js
+++ data/myapp/src/App.js
@@ -4,15 +4,16 @@ class App extends Component {
 	constructor(props) {
 		super();
 		this.state = {
-			text: 'Hi!',
+			name: { firstName: 'Jacob', lastName: 'Rose' },
 		};
 	}
 	//prettier-ignore
 	render() {
     return (
 		<div>
-			<div>{this.state.text}</div>
-			<button>Change text</button>
+        <div>{this.state.name.firstName} { this.state.name.lastName}</div>
+        <button onClick={() => { this.setState({ name: { firstName: 'Robert', lastName: 'Kowalsky'}})} }>Change </button>
+        <button onClick={() => { this.setState({ name: { firstName: 'Margaret', lastName: 'Jackson'}})} }>Change 2</button>
 		</div>
 	);  
 	}
