c266f57a46d47fba8dd8a297078a8ac0b461eecd
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-04-15 22:30:08 +0200

    update

--- data/myapp/src/App.js
+++ data/myapp/src/App.js
@@ -1,58 +1,85 @@
 import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			monsters: [],
-			searchField: '',
-		};
-	}
+// functional components
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) => {
-				this.setState(
-					// when update is complete react have to ReRender again
-					() => {
-						return { monsters: users };
-					}
-				);
-			});
-	}
+const App = () => {
+	const [searchField, setSearchField] = useState(''); // [value, setValue]
 
-	onSearchChange = (e) => {
-		const searchField = e.target.value.toLowerCase();
-
-		this.setState(() => {
-			return { searchField };
-		});
+	const onSearchChange = (e) => {
+		const searchFieldString = e.target.value.toLowerCase();
+		setSearchField(searchFieldString);
 	};
 
-	render() {
-		const { monsters, searchField } = this.state;
-		const { onSearchChange } = this;
-		const filteredMonsters = monsters.filter((monster) => {
-			return monster.name.toLowerCase().includes(searchField);
-		});
+	return (
+		<div className='App'>
+			<h1 className='app-title'>Monsters Rolodex</h1>
+			<SearchBox
+				className={'search-box'}
+				onChangeHanlder={onSearchChange}
+				placeholder={'search'}
+			/>
+			{/* <CardList monsters={filteredMonsters} /> */}
+		</div>
+	);
+};
 
-		return (
-			<div className='App'>
-				<h1 className='app-title'>Monsters Rolodex</h1>
-				<SearchBox
-					className={'search-box'}
-					onChangeHanlder={onSearchChange}
-					placeholder={'search'}
-				/>
-				<CardList monsters={filteredMonsters} />
-			</div>
-		);
-	}
-}
+// PURE FUNCTION - independent, nothing affects on that function
+// IMPURE FUNCTION - create some kind of effect outside of the scope
+
+// class App extends Component {
+// 	constructor(props) {
+// 		super();
+// 		this.state = {
+// 			monsters: [],
+// 			searchField: '',
+// 		};
+// 	}
+
+// 	componentDidMount() {
+// 		fetch('https://jsonplaceholder.typicode.com/users')
+// 			.then((response) => response.json())
+// 			.then((users) => {
+// 				this.setState(
+// 					// when update is complete react have to ReRender again
+// 					() => {
+// 						return { monsters: users };
+// 					}
+// 				);
+// 			});
+// 	}
+
+// 	onSearchChange = (e) => {
+// 		const searchField = e.target.value.toLowerCase();
+
+// 		this.setState(() => {
+// 			return { searchField };
+// 		});
+// 	};
+
+// 	render() {
+// 		const { monsters, searchField } = this.state;
+// 		const { onSearchChange } = this;
+// 		const filteredMonsters = monsters.filter((monster) => {
+// 			return monster.name.toLowerCase().includes(searchField);
+// 		});
+
+// 		return (
+// 			<div className='App'>
+// 				<h1 className='app-title'>Monsters Rolodex</h1>
+// 				<SearchBox
+// 					className={'search-box'}
+// 					onChangeHanlder={onSearchChange}
+// 					placeholder={'search'}
+// 				/>
+// 				<CardList monsters={filteredMonsters} />
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default App;
 
