0cc397d08c1fb3a83f6173339fea6a440948ad00
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-05-02 23:08:38 +0200

    update

--- data/ecommerceApp/src/routes/sign-in/sign-in.component.jsx
+++ data/ecommerceApp/src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,26 @@
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+	// useEffect(async () => {
+	// 	const response = await getRedirectResult(auth);
+	// 	if (response) {
+	// 		const userDocRef = await createUserDocumentFromAuth(response.user);
+	// 	}
+	// }, []);
+
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
-		createUserDocumentFromAuth(user);
+		const userDocRef = await createUserDocumentFromAuth(user);
 	};
 
 	return (
 		<div>
 			<h1>Sign in Page</h1>
 			<button onClick={logGoogleUser}>Sign In with Google Popup</button>
+			{/* <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
 		</div>
 	);
 };
--- data/ecommerceApp/src/utils/firebase/firebase.utils.js
+++ data/ecommerceApp/src/utils/firebase/firebase.utils.js
@@ -21,6 +21,7 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
@@ -32,4 +33,21 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 	const userSnapshot = await getDoc(userDocRef);
 	console.log(userSnapshot);
 	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (e) {
+			console.log(`error creating the user ${e.message}`);
+		}
+	}
+
+	return userDocRef;
 };
