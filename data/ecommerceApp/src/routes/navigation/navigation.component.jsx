b18c1ea56b3737448bd976336104c27a7d7109e3
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-05-07 23:17:21 +0200

    update

--- data/ecommerceApp/src/routes/navigation/navigation.component.jsx
+++ data/ecommerceApp/src/routes/navigation/navigation.component.jsx
@@ -17,6 +17,9 @@ const Nav = () => {
 					<CrwnLogo className='logo' />
 				</Link>
 				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
 					{currentUser ? (
 						<span className='nav-link' onClick={signOutUser}>
 							SIGN OUT
