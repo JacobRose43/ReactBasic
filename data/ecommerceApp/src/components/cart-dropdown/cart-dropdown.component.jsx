50d0fb75e95f8ad2e99ad36a63e6f3456044d62f
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-05-12 21:05:48 +0200

    update

--- data/ecommerceApp/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ data/ecommerceApp/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,19 @@
-import Button from '../button/button.component'
+import Button from '../button/button.component';
+
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.style.scss';
 
 const CartDropdown = () => {
-    return (
-        <div className="cart-dropdown-container">
-            
-        </div>
-    )
+	return (
+		<div className='cart-dropdown-container'>
+			<div className='cart-items'>
+				{[].map((item) => (
+					<CartItem cartItem={item} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default CartDropdown;
