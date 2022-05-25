a769e2e180cc40b58549fadbf17c55c4653ae3f6
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-05-25 23:10:07 +0200

    update

--- data/ecommerceApp/src/components/product-card/product.card.component.jsx
+++ data/ecommerceApp/src/components/product-card/product.card.component.jsx
@@ -16,7 +16,7 @@ const ProductCard = ({ product }) => {
 			<img src={imageUrl} alt={`${name}`} />
 			<div className='footer'>
 				<span className='name'>{name}</span>
-				<span className='price'>{price}</span>
+				<span className='price'>${price}</span>
 			</div>
 			<Button buttonType={BUTTON_TYPES_CLASSES.inverted} onClick={addProductToCart}>
 				Add to card
